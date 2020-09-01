79b4e06b64225aef04733756aaa783aa80398e30
librelois <c@elo.tf>
2020-09-02 00:19:02 +0200

    pub: create publish script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cargo publish --manifest-path crypto/Cargo.toml || exit 1
+sleep 5
+cargo publish --manifest-path common/Cargo.toml || exit 1
+sleep 5
+cargo publish --manifest-path wallet/Cargo.toml || exit 1
+sleep 5
+cargo publish --manifest-path documents/Cargo.toml || exit 1
+sleep 5
+cargo publish --manifest-path documents-parser/Cargo.toml || exit 1
+sleep 5
+cargo publish --manifest-path block/Cargo.toml || exit 1
