b169d1be9589cbc8ceb61a0870f7595b57bb9ef1
librelois <c@elo.tf>
2020-09-01 15:23:54 +0200

    fix(parser): must be parse any document

--- documents-parser/src/raw_text/document.rs
+++ documents-parser/src/raw_text/document.rs
@@ -22,17 +22,36 @@ pub fn dubp_doc_from_pest_pair_v10(pair: Pair<Rule>) -> Result<DubpDocument, Raw
         Rule::membership_v10 => Ok(DubpDocument::Membership(MembershipDocument::V10(
             MembershipDocumentV10::from_pest_pair(doc_type_v10_pair)?,
         ))),
-        Rule::cert_v10 => Ok(DubpDocument::Certification(Box::new(
-            CertificationDocument::V10(CertificationDocumentV10::from_pest_pair(
-                doc_type_v10_pair,
-            )?),
+        Rule::cert_v10 => Ok(DubpDocument::Certification(CertificationDocument::V10(
+            CertificationDocumentV10::from_pest_pair(doc_type_v10_pair)?,
         ))),
-        Rule::revoc_v10 => Ok(DubpDocument::Revocation(Box::new(
-            RevocationDocument::from_pest_pair(doc_type_v10_pair)?,
+        Rule::revoc_v10 => Ok(DubpDocument::Revocation(RevocationDocument::V10(
+            RevocationDocumentV10::from_pest_pair(doc_type_v10_pair)?,
         ))),
-        Rule::tx_v10 => Ok(DubpDocument::Transaction(Box::new(
-            TransactionDocument::from_pest_pair(doc_type_v10_pair)?,
+        Rule::tx_v10 => Ok(DubpDocument::Transaction(TransactionDocument::V10(
+            TransactionDocumentV10::from_pest_pair(doc_type_v10_pair)?,
         ))),
         _ => panic!("unexpected rule: {:?}", doc_type_v10_pair.as_rule()), // Grammar ensures that we never reach this line
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_any_document() {
+        let doc = "Version: 10
+Type: Revocation
+Currency: g1
+Issuer: DNann1Lh55eZMEDXeYt59bzHbA3NJR46DeQYCS2qQdLV
+IdtyUniqueID: tic
+IdtyTimestamp: 0-E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855
+IdtySignature: 1eubHHbuNfilHMM0G2bI30iZzebQ2cQ1PC7uPAw08FGMMmQCRerlF/3pc4sAcsnexsxBseA/3lY03KlONqJBAg==
+XXOgI++6qpY9O31ml/FcfbXCE6aixIrgkT5jL7kBle3YOMr+8wrp7Rt+z9hDVjrNfYX2gpeJsuMNfG4T/fzVDQ==";
+
+        let doc = DubpDocument::parse_from_raw_text(doc).expect("fail to parse any document");
+        println!("Doc : {:?}", doc);
+        assert!(doc.verify_signatures().is_ok())
     }
 }
