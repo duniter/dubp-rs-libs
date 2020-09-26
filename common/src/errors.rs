655a3b5c6e7e51c9dbe9267c82c4534ce8ec3c57
librelois <c@elo.tf>
2020-09-26 19:53:26 +0200

    feat (common): impl Error for DocumentSigsErr

--- common/src/errors.rs
+++ common/src/errors.rs
@@ -7,12 +7,14 @@ use crate::*;
 pub struct StringErr(pub String);
 
 /// List of possible errors for document signatures verification.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Error, Eq, PartialEq)]
 pub enum DocumentSigsErr {
     /// Not same amount of issuers and signatures.
     /// (issuers count, signatures count)
+    #[error("Not same amount of issuers and signatures: found {0} issuers and {1} signatures.")]
     IncompletePairs(usize, usize),
     /// Signatures don't match.
     /// List of mismatching pairs indexes.
+    #[error("Signatures don\'t match: {0:?}")]
     Invalid(HashMap<usize, SigError>),
 }
