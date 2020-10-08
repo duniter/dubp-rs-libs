9b3a9cbc1215ef12ba243c6964310dbc234fa1ef
librelois <c@elo.tf>
2020-10-09 00:20:52 +0200

    pub: need to sleep more

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ sleep 15
 cargo publish --manifest-path wallet/Cargo.toml || exit 1
 sleep 15
 cargo publish --manifest-path documents/Cargo.toml || exit 1
-sleep 15
+sleep 20
 cargo publish --manifest-path documents-parser/Cargo.toml || exit 1
-sleep 15
+sleep 20
 cargo publish --manifest-path block/Cargo.toml || exit 1
