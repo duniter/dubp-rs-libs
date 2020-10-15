d43049b2ac9c4b508e0c4b40bf3ca806d94fd497
librelois <c@elo.tf>
2020-10-15 02:30:11 +0200

    feat(common): impl zerocopy & FromStr for BlockNumber

--- common/src/block_number.rs
+++ common/src/block_number.rs
@@ -2,12 +2,33 @@ use crate::*;
 
 /// A block number.
 #[derive(
-    Copy, Clone, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize,
+    Copy,
+    Clone,
+    Debug,
+    Default,
+    Deserialize,
+    Eq,
+    Hash,
+    Ord,
+    PartialEq,
+    PartialOrd,
+    Serialize,
+    zerocopy::AsBytes,
+    zerocopy::FromBytes,
 )]
+#[repr(transparent)]
 pub struct BlockNumber(pub u32);
 
 impl Display for BlockNumber {
     fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
         write!(f, "{}", self.0)
+    }
+}
+
+impl FromStr for BlockNumber {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        u32::from_str(s).map(BlockNumber)
     }
 }
