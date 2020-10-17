674e65cce8c96d9ddf56d843cd7c9e46772149df
librelois <c@elo.tf>
2020-10-17 17:06:57 +0200

    build: publish: back to sleep between crates

--- publish.sh
+++ publish.sh
@@ -3,25 +3,14 @@
 VERSION_NAME=${CI_COMMIT_TAG#v}
 echo "publish v$VERSION_NAME ..."
 
-wait_crate() {
-    v=$(curl -s "https://crates.io/api/v1/crates/${1}" | jq '.crate.max_version')
-    while [ $v != "\"$VERSION_NAME\"" ]
-    do
-    echo "${1} not updated, wait …"
-    echo $v
-    sleep 5
-    v=$(curl -s "https://crates.io/api/v1/crates/${1}" | jq '.crate.max_version')
-    done
-}
-
 cargo publish --manifest-path crypto/Cargo.toml || exit 1
-wait_crate "dup-crypto"
+sleep 30
 cargo publish --manifest-path common/Cargo.toml || exit 1
-wait_crate "dubp-common"
+sleep 30
 cargo publish --manifest-path wallet/Cargo.toml || exit 1
-wait_crate "dubp-wallet"
+sleep 30
 cargo publish --manifest-path documents/Cargo.toml || exit 1
-wait_crate "dubp-documents"
+sleep 30
 cargo publish --manifest-path documents-parser/Cargo.toml || exit 1
-wait_crate "dubp-documents-parser"
+sleep 30
 cargo publish --manifest-path block/Cargo.toml || exit 1
