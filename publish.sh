68156f5c2dc6450ea6e62f7d5b6bd719860aacf5
librelois <c@elo.tf>
2021-05-19 17:45:24 +0200

    build(publish): add crates peer & bda-types

--- publish.sh
+++ publish.sh
@@ -7,6 +7,8 @@ cargo publish --manifest-path crypto/Cargo.toml || exit 1
 sleep 30
 cargo publish --manifest-path common/Cargo.toml || exit 1
 sleep 30
+cargo publish --manifest-path peer/Cargo.toml || exit 1
+sleep 30
 cargo publish --manifest-path wallet/Cargo.toml || exit 1
 sleep 30
 cargo publish --manifest-path documents/Cargo.toml || exit 1
@@ -14,5 +16,7 @@ sleep 30
 cargo publish --manifest-path documents-parser/Cargo.toml || exit 1
 sleep 30
 cargo publish --manifest-path block/Cargo.toml || exit 1
+sleep 30
+cargo publish --manifest-path bda-types/Cargo.toml || exit 1
 sleep 30
 cargo publish --manifest-path Cargo.toml || exit 1
